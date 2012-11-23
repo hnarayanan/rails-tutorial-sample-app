a645bf7e7b8f57066fd3cd659a07fa818a2e3cd1
Harish Narayanan <hnarayanan@gmail.com>
2012-11-23 22:29:54 +0100

    Downcase e-mail before storing in DB to further ensure uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
 
+  before_save { |user| user.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /^[\w+\-.]+@[a-z\d\-.]+\.[a-z]+$/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
