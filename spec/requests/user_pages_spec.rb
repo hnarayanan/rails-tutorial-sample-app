3f96b63021593c0100bfa5593509639b4c09f2ee
Harish Narayanan <hnarayanan@gmail.com>
2012-11-25 13:31:26 +0100

    Add more nuanced tests for failed signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,14 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+      describe "after submision" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content("Email can't be blank") }
+        it { should have_content("Name can't be blank") }
+        it { should have_content("Password can't be blank") }
+      end
     end
 
     describe "with valid information" do
